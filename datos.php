e742e389b2add09e1213832efdc90b9acd074dff
Omar09124 <136379197+Omar09124@users.noreply.github.com>
2023-06-28 00:36:57 -0600

    Archivos terminados
    Los archivos estan completamente terminados, solo falta centrar y cambiar tamaño a los campos de los formularios.

--- datos.php
+++ datos.php
@@ -13,5 +13,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $sitio_web = $_GET["txtSitioWeb"];
   $asunto = $_GET["txtAsunto"];
   $mensaje = $_GET["txtMensaje"];*/
+
+  echo "Los datos se han guardado correctamente:<br>";
+  echo "Nombre: " . $nombre . "<br>";
+  echo "Email: " . $email . "<br>";
+  echo "Teléfono: " . $telefono . "<br>";
+  echo "SitioWeb: " . $sitio_web . "<br>";
+  echo "Asunto: " . $asunto . "<br>";
+  echo "Mensaje: " . $mensaje . "<br>";
 }
 ?>
